35504ebccd61dcaf0d5f2187b9b8989135319990
JonHelmus <jonathanchelmus@gmail.com>
2017-12-07 21:28:02 -0800

    stuffs

---
+++ stuffs.py
@@ -0,0 +1,5 @@
+import base64; print('9b0f7b43804d4abd6f7e1bbe51
+5c55d5'.decode('hex'))" | /problems/33432c6de9329bca3a3ff26e
+5538d8f2/hex2raw
+Give me this in raw form (0x41 -> 'A'):                     
+9b0f7b43804d4abd6f7e1bbe515c55d5 
